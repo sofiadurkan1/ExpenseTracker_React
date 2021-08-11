c05892a495e313486c6e5fafe9ad1b12641e0302
sofia durkan <sofiadurkan@gmail.com>
2021-08-10 21:35:43 -0500

    expensedate added

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -26,7 +26,6 @@ const expenses = [
 ];
   return (
     <div className="App">
-      <h2>Let s get started</h2>
       <ExpenseItem title={expenses[0].title} amount = {expenses[0].amount} date ={expenses[0].date}/>
       <ExpenseItem title={expenses[1].title} amount = {expenses[1].amount} date ={expenses[1].date}/>
       <ExpenseItem title={expenses[2].title} amount = {expenses[2].amount} date ={expenses[2].date}/>
--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 
 function ExpenseItem(props){
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.90;
+    
     return  (
         <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
+    
     <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
