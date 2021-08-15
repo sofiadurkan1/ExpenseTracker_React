34015ab60244fc421c3e44ef83a1e0e9ffd8fd46
sofia durkan <sofiadurkan@gmail.com>
2021-08-15 15:58:11 -0500

    expenselist added

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card'
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses =  (props) =>{
 
@@ -16,21 +16,12 @@ const Expenses =  (props) =>{
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length>0){
-    expensesContent = filteredExpenses.map((expense) =>(
-      <ExpenseItem 
-      key ={expense.id}
-      title={expense.title} 
-      amount ={expense.amount} 
-      date={expense.date}/>))
-      }
   
     return(
       <div>
         <Card className="expenses">
         <ExpensesFilter selected ={filteredYear} onChangeFilter = {filterChangeHandler}/>
-        {expensesContent}
+        <ExpensesList items ={filteredExpenses}/>
         
         
 
